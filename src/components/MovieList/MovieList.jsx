8b71b3df97c86b5c4ca1e4b7419de597432bb66e
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-29 20:34:48 -0500

    created function to handle details page nav

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';}
 import './MovieList.css';
 
 function MovieList() {
 
+  const history = useHistory();
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
+
+  const handleClick = (event, movie) => {
+    history.push({ pathname: '/details', state: movie});
+
+  }
 
   return (
     <main>
@@ -19,7 +26,7 @@ function MovieList() {
           return (
             <div data-testid='movieItem' key={movie.id}>
               <h3>{movie.title}</h3>
-              <img data-testid='toDetails' src={movie.poster} alt={movie.title}/>
+              <img data-testid='toDetails' src={movie.poster} alt={movie.title} onClick={(event) => handleClick(movie)}/>
             </div>
           );
         })}
